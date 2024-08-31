d7a3301f5e1e947da0eedf449427bb2c3d3ec44c
Nico <nico.esg.rey@gmail.com>
2024-08-31 23:49:30 +0100

    Added stdio.h and fixed compilation warnings

--- main.c
+++ main.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
-int main(){
-    printf("testing");
+#include <GLFW/glfw3.h>
+
+int main() {
+    if (!glfwInit()) {
+        printf("Failed to initialize GLFW\n");
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Your Bonfire", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        printf("Failed to create GLFW window\n");
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Render here
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
